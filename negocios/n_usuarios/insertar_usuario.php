c103dfd53be6e1cbb2f4387a4da802ffa300e9d0
Sebas175 <sebastianmontero175@gmail.com>
2021-02-12 11:18:09 -0600

    Generación de contraseña

--- negocios/n_usuarios/insertar_usuario.php
+++ negocios/n_usuarios/insertar_usuario.php
@@ -2,7 +2,7 @@
 
     require("../../data/data_usuarios.php");
 
-    if(!empty($_POST["cedula"]) && !empty($_POST["nombre_usuario"]) && !empty($_POST["apell1"]) && !empty($_POST["apell2"]) && !empty($_POST["email"]) && !empty($_POST["cel_1"]) && !empty($_POST["cel_2"]) && !empty($_REQUEST["rol"])){
+    if(!empty($_POST["cedula"]) && !empty($_POST["nombre_usuario"]) && !empty($_POST["apell1"]) && !empty($_POST["apell2"]) && !empty($_POST["email"]) && !empty($_POST["cel_1"]) && !empty($_REQUEST["rol"])){
 
         echo "Exito <br>";
 
@@ -15,9 +15,9 @@
         $celular_op = $_POST["cel_2"];
         $rol = $_REQUEST["rol"];
 
-        $contra = "123";
-
         $usuarios = new D_Usuarios();
+        $contra = generar_contra();
+        $contra = password_hash($contra, PASSWORD_DEFAULT, ["cost"=>5]);
         $insertarUsuario = $usuarios->insertarUsuario($cedula,$nombre,$apellido1,$apellido2,$email,$celular,$celular_op,$contra);
         $insertarRol = $usuarios->insertarRolUsuario($rol,$cedula);
         
@@ -25,13 +25,23 @@
 
             echo "Inserción exitosa en la tabla Usuarios y rol-usuarios <br>";
             
-
-
         }else{
             echo "Error en la inserción <br>";
         }
 
     }else{
         echo "Fallo";
+    }
+
+
+    function generar_contra(){
+        $caracteres='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789?!¡%$#¿*';
+        $longpalabra=4;
+        for($pass='', $n=strlen($caracteres)-1; strlen($pass) < $longpalabra ; ) {
+            $x = rand(0,$n);
+            $pass.= $caracteres[$x];
+        }
+        return $pass;
+
     }
 ?>
