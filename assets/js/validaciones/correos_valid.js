ef4dd52e5b6aebf4bce7ffe3deec5a77c241b29e
Alvaro499 <alvarosiles499@gmail.com>
2021-02-17 20:56:07 -0600

    Ortografia Validacion Correo

--- assets/js/validaciones/correos_valid.js
+++ assets/js/validaciones/correos_valid.js
@@ -9,7 +9,7 @@ function validacion(){
 
 	if (asunto == null || asunto == "" || asunto.length > 100) {
 
-		document.getElementById("error_asunto").innerHTML = "El correo debe llevar un asunto y ser menor a 100 carácteres";
+		document.getElementById("error_asunto").innerHTML = "El asunto es obligatorio";
 		document.getElementById("error_asunto").style.color = "#E40017";
 		document.getElementById("asunto_correos").style.border = "3px solid #E40017";
 		cant_error++;
@@ -26,7 +26,7 @@ function validacion(){
 
 	if (correo == null || correo == "") {
 
-		document.getElementById("error_correo").innerHTML = "El correo debe llevar un mensaje antes enviarse";
+		document.getElementById("error_correo").innerHTML = "El correo es obligatorio";
 		document.getElementById("error_correo").style.color = "#E40017";
 		document.getElementById("mensaje_correos").style.border = "3px solid #E40017";
 		cant_error++;
