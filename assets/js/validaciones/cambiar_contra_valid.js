e0b2b7fb3077d4b889728b1063426794b17db66f
Alvaro499 <alvarosiles499@gmail.com>
2021-02-18 13:45:30 -0600

    Probando Validaciones Segunda Vez

--- assets/js/validaciones/cambiar_contra_valid.js
+++ assets/js/validaciones/cambiar_contra_valid.js
@@ -1,5 +1,5 @@
 "use strict";
-
+console.log("hola");
 function validacion(){
 	var contra_nueva = document.getElementById("contra_nueva").value;
 	var confirm_contra = document.getElementById("confirm_contra").value;
@@ -7,7 +7,7 @@ function validacion(){
 
 	//var regex_texto = new RegExp('^[a-zA-ZÀ-ÿ]+$', 'i', 'g');
 
-	if (contra_nueva == null || contra_nueva == "") {
+	if (contra_nueva == null || contra_nueva == "" || contra_nueva.length < 6 || contra_nueva.length > 12) {
 
 		document.getElementById("error_contra_nueva").innerHTML = "Su nueva contraseña debe incluir carácteres alfanuméricos; y ser de 6 a 12 caráctres";
 		document.getElementById("error_contra_nueva").style.color = "#E40017";
@@ -18,15 +18,9 @@ function validacion(){
 
 		document.getElementById("error_contra_nueva").innerHTML = "";
 		document.getElementById("contra_nueva").style.border = "3px solid #54E346";
-
-		if (cant_error > 0) {
-
-			cant_error--;
-		}
-
 	}
 
-	if (confirm_contra == "" || !confirm_contra === contra_nueva) {
+	if (confirm_contra != contra_nueva) {
 
 		document.getElementById("error_confirm_contra").innerHTML = "La contraseña no coincide, verificar nuevamente";
 		document.getElementById("error_confirm_contra").style.color = "#E40017";
@@ -37,12 +31,6 @@ function validacion(){
 
 		document.getElementById("error_confirm_contra").innerHTML = "";
 		document.getElementById("confirm_contra").style.border = "3px solid #54E346";
-
-		if (cant_error > 0) {
-
-			cant_error--;
-		}
-
 	}
 
 	if(cant_error>0){
