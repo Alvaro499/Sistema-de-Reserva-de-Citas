58989b3ac47204691d84d18e3dad463a2ce5dba7
Alvaro499 <alvarosiles499@gmail.com>
2021-02-19 22:53:06 -0600

    Final de Diseno Errores de Validacion

--- assets/js/validaciones/rcc_valid.js
+++ assets/js/validaciones/rcc_valid.js
@@ -14,7 +14,7 @@ function validacion(){
     //var regex_correo = new RegExp(/\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)/);
 
     if (asunto == null || asunto == "") {
-        document.getElementById("error_asunto").innerHTML = "El Asunto es obligatorio.";
+        document.getElementById("error_asunto").innerHTML = "*El Asunto es obligatorio.";
         document.getElementById("error_asunto").style.color = "#C70039";
         document.getElementById("asunto").style.border = "3px solid #E40017";
         cant_error++;
@@ -28,7 +28,7 @@ function validacion(){
     }
     
     if (mensaje == null || mensaje == "") {
-        document.getElementById("error_mensaje").innerHTML = "El Mensaje es obligatorio.";
+        document.getElementById("error_mensaje").innerHTML = "*El Mensaje es obligatorio.";
         document.getElementById("error_mensaje").style.color = "#C70039";
         document.getElementById("mensaje_ct").style.border = "3px solid #E40017";
         cant_error++;
@@ -42,7 +42,7 @@ function validacion(){
     }
     
     if (fecha == null || fecha == "") {
-        document.getElementById("error_fecha").innerHTML = "La fecha es obligatoria";
+        document.getElementById("error_fecha").innerHTML = "*La fecha es obligatoria";
         document.getElementById("error_fecha").style.color = "#C70039";
         document.getElementById("fecha").style.border = "3px solid #E40017";
         cant_error++;
@@ -56,7 +56,7 @@ function validacion(){
     }
     
     if (hora == null || hora == "") {
-        document.getElementById("error_hora").innerHTML = "La Hora es obligatoria.";
+        document.getElementById("error_hora").innerHTML = "*La Hora es obligatoria.";
         document.getElementById("error_hora").style.color = "#C70039";
         document.getElementById("hora").style.border = "3px solid #E40017";
         cant_error++;
