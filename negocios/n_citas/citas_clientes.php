a44ceeaa03ed2e92f0ec39f575027ba7e9f7389d
Sebas175 <sebastianmontero175@gmail.com>
2021-03-08 22:23:32 -0600

    Subir archivo

--- negocios/n_citas/citas_clientes.php
+++ negocios/n_citas/citas_clientes.php
@@ -14,12 +14,29 @@ $citas = new D_Citas();
 $sesion = new C_Sesion();
 $sesion->inicializar();
 
+if(!empty($archivo)){
+    subirImagen($archivo);
+}
 $result= $citas->insertarCitas($area, $asunto, $mensaje, $fecha, $hora,$medio, $archivo,$_SESSION["cedula"]);
 
 if($result){
     echo 1;
 }else{
     echo 0;
+}
+
+function subirImagen($funcionImagen){
+    $gt = $_FILES["file"]["tmp_name"];
+    if (is_uploaded_file($gt)) {
+        $ruta = "../../assets/archivos/";
+        $upload = $ruta . $funcionImagen;
+        if (move_uploaded_file($gt, $upload)) {
+        }else{
+            echo 3;
+        }
+    }else{
+        echo 4;
+    }
 }
 
 ?>
