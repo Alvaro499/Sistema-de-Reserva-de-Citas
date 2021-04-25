875585c7abe249927a18b4302e277753d14f4b0e
Sebas175 <sebastianmontero175@gmail.com>
2021-04-25 10:08:45 -0600

    Posición y Duración de las alertas parte 2

--- assets/js/foto_perfil.js
+++ assets/js/foto_perfil.js
@@ -3,35 +3,35 @@ console.log("HOLA MUNDO 1.0");
 let modal = document.querySelector(".modal_container"); //contenedor
 let modal_form = document.querySelector(".modal"); //formulario
 
-    // ABRIR MODAL
+// ABRIR MODAL
 let abrir_modal = document.querySelector(".label_foto");
 
 console.log(abrir_modal);
-abrir_modal.addEventListener("click", function () {
+abrir_modal.addEventListener("click", function() {
 
     modal.style.visibility = "initial";
     modal.style.transform = "scale(1,1)";
     modal.style.transition = "transform 0.6s ease";
     if (modal.style.transform = "scale(1,1)") {
         setTimeout(() => {
-            modal.style.background = "rgba(0,0,0,0.7)";    
+            modal.style.background = "rgba(0,0,0,0.7)";
             modal.style.transition = "background 0.6s ease";
-        }, 400);        
+        }, 400);
     }
 });
 
-    // CERRAR MODAL
+// CERRAR MODAL
 let cerrar_modal = document.querySelectorAll(".cerrar_modal");
 for (let i = 0; i < cerrar_modal.length; i++) {
-    
-    cerrar_modal[i].addEventListener("click", function (e) {
+
+    cerrar_modal[i].addEventListener("click", function(e) {
         // modal.style.display = "none";
         modal.style.visibility = "hidden";
         modal.style.transform = "scale(0.1,0.1)";
-        modal.style.background = "rgba(0,0,0,0)";    
-        window.location.reload(); 
+        modal.style.background = "rgba(0,0,0,0)";
+        window.location.reload();
     });
-    
+
 }
 // for(let opciones in cerrar_modal){
 //     cerrar_modal[opciones].addEventListener("click", function (e) {
@@ -50,25 +50,25 @@ let input = document.querySelector("#agregar_foto");
 console.log(input);
 //let nombre_archivo = document.querySelector("#nombre_archivo");
 
-input.onchange = function(){
+input.onchange = function() {
     var archivo = this.files[0];
     console.log(archivo);
 
     if (archivo.type == "image/png" || archivo.type == "image/svg" || archivo.type == "image/jpg" || archivo.type == "image/jpeg") {
 
         if (archivo.size >= 250000000) {
-            
-            toastr.error("El tamaño del archivo excede los 25MB", "Error");
+
+            toastr.error("El tamaño del archivo excede los 25MB", "Error", { positionClass: "toast-bottom-right", showDuration: "400" });
             //$("#agregar_foto").val('');
             this.value = "";
-        
-        }else{
+
+        } else {
             // toastr.success("El tamaño del archivo excede los 25MB", "Error";
         }
-    }else{
-        toastr.error("Formato de imagen no permitido", "Error");
+    } else {
+        toastr.error("Formato de imagen no permitido", "Error", { positionClass: "toast-bottom-right", showDuration: "400" });
         //$("#agregar_foto").val('');
         this.value = "";
     }
-    
+
 };
--- assets/js/upload_file.js
+++ assets/js/upload_file.js
@@ -1,6 +1,6 @@
 //Cuando el input type file detecte un cambio, nos traemos el nombre del archivo seleccionado, pero la ruta no es la verdadera, ya que incluye "fake/path/"
 
-var form =  document.getElementById("formu");
+var form = document.getElementById("formu");
 var input_file = document.getElementById('file');
 var file_name = document.getElementById('file_name');
 var list_names = "";
@@ -9,17 +9,17 @@ input_file.onchange = function() {
 
     var archi = this.files;
     if (archi.length <= 6) {
-    
+
         // console.log(archi[1]);
         // console.log(archi[1].type);
         // var archiv = archi.type;
         // console.log(archiv);
         //console.log(this.value); //regresa "fake"
         for (let index_type = 0; index_type < archi.length; index_type++) {
-            
+
             if (archi[index_type].type == "image/jpg" || archi[index_type].type == "image/jpeg" || archi[index_type].type == "image/png" || archi[index_type].type == "application/pdf" || archi[index_type].type == "application/msword" || archi[index_type].type == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" || archi[index_type].type == "application/vnd.ms-excel" || archi[index_type].type == "application/vnd.openxmlformats-officedocument.wordprocessingml.document") {
                 if (archi.size >= 500000000) {
-                    toastr.error("Los archivos no pueden superar los 25MB de tamaño", "Error");
+                    toastr.error("Los archivos no pueden superar los 25MB de tamaño", "Error", { positionClass: "toast-bottom-right", showDuration: "400" });
                     file_name.innerHTML = "";
                     $("#file").val('');
                 } else {
@@ -33,18 +33,18 @@ input_file.onchange = function() {
                     // file_name.innerHTML = input_file.files[0].name;
                 }
             } else {
-                toastr.error("Formato no permitido", "Error");
+                toastr.error("Formato no permitido", "Error", { positionClass: "toast-bottom-right", showDuration: "400" });
                 file_name.innerHTML = "";
                 $("#file").val('');
             }
 
-        }//cierre del for
+        } //cierre del for
 
-    }else{
+    } else {
         toastr.error("No se pueden enviar más de 6 archivos", "Error");
         file_name.innerHTML = "";
         $("#file").val('');
 
-    }//cierre del primer if
+    } //cierre del primer if
 
 }
