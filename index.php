c8bcd44743b8465e26ea5a4d00d57cd3a01a98ce
Alvaro499 <alvarosiles499@gmail.com>
2026-01-10 15:48:34 -0600

    ReadMe - .htaccess activado - Index de Reenvío

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Página de entrada principal
+ * Redirige a la página de login
+ */
+
+// Redirigir a la página de inicio de sesión
+header("Location: ui/login/sesion.php");
+exit();
+?>
